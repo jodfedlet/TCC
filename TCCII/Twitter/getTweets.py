9d9094d8d7eb7914d4e11df2e8dc3100c2a9a5c6
Jod Fedlet PIERRE <fedletpierre15@gmail.com>
2022-05-04 23:35:22 -0300

    Update getTweets.py

--- TCCII/Twitter/getTweets.py
+++ TCCII/Twitter/getTweets.py
@@ -14,15 +14,8 @@ list_of_teets = []
 for idx, tweet in enumerate(scraper.get_items()):
   # if tweet.date.year < 2018 or (tweet.date.year == 2022 and tweet.date.month > 5):
   #   break
-
-  #if idx > 50: break
-  # print('-------------------------------------------')
-  # print(tweet.id)
-  # print(tweet.date)
-  # print(tweet.content)
   tweet_dict = {'datetime':tweet.date, 'tweet_id': tweet.id,  'user_id': tweet.user.id, 'text': tweet.content}
   list_of_teets.append(tweet_dict)
-  # print('-------------------------------------------')
   count = idx
 print(count)
 #result = 185221
