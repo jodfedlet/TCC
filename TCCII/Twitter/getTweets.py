8d72c7aefff6c51b40ff526cbe1981c48e1ac9d3
jodfedlet <fedletpierre15@gmail.com>
2022-05-04 23:31:42 -0300

    Get tweets

---
+++ TCCII/Twitter/getTweets.py
@@ -0,0 +1,31 @@
+
+
+#https://github.com/JustAnotherArchivist/snscrape/blob/master/README.md
+
+
+import pandas as pd
+import snscrape.modules.twitter as twitterScraper
+
+scraper = twitterScraper.TwitterSearchScraper(('haiti OR haitiano OR haitiana lang:pt since:2018-01-01 until:2022-05-04'))
+
+count = 0
+
+list_of_teets = []
+for idx, tweet in enumerate(scraper.get_items()):
+  # if tweet.date.year < 2018 or (tweet.date.year == 2022 and tweet.date.month > 5):
+  #   break
+
+  #if idx > 50: break
+  # print('-------------------------------------------')
+  # print(tweet.id)
+  # print(tweet.date)
+  # print(tweet.content)
+  tweet_dict = {'datetime':tweet.date, 'tweet_id': tweet.id,  'user_id': tweet.user.id, 'text': tweet.content}
+  list_of_teets.append(tweet_dict)
+  # print('-------------------------------------------')
+  count = idx
+print(count)
+#result = 185221
+
+df = pd.DataFrame(list_of_teets)
+df.to_csv('all_tweets.csv')
