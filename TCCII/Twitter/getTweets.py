5bdb706c5ede0de664a43a6527928c8f2d9157f4
jodfedlet <fedletpierre15@gmail.com>
2022-06-09 11:09:17 -0300

    ...

--- TCCII/Twitter/getTweets.py
+++ TCCII/Twitter/getTweets.py
@@ -6,18 +6,15 @@
 import pandas as pd
 import snscrape.modules.twitter as twitterScraper
 
-scraper = twitterScraper.TwitterSearchScraper(('haiti OR haitiano OR haitiana lang:pt since:2018-01-01 until:2022-05-04'))
+query=('haiti OR haitiano OR haitiana lang:pt since:2018-01-01 until:2022-05-31 -filter:replies')
 
-count = 0
+scraper = twitterScraper.TwitterSearchScraper(query)
+
 list_of_teets = []
 for idx, tweet in enumerate(scraper.get_items()):
-  # if tweet.date.year < 2018 or (tweet.date.year == 2022 and tweet.date.month > 5):
-  #   break
   tweet_dict = {'datetime':tweet.date,'tweet_id':tweet.id,'user_id':tweet.user.id,'text':tweet.content}
   list_of_teets.append(tweet_dict)
-  count = idx
-print(count)
-#result = 185221
 
 df = pd.DataFrame(list_of_teets)
-df.to_csv('all_tweets.csv')
+print(len(df))
+df.to_csv('csv/all_tweets_without_retweets.csv')
